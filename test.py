bdab9ee66a7e3458adc409e1996a10362c9651f6
MarkTheF4rth <mnel.jnr@gmail.com>
2016-07-08 21:52:12 +0100

    Improved test.py to allow server to be inputted through console and take the token from a txt file, it will also inform the player when either of these are not provided.

--- test.py
+++ test.py
@@ -1,13 +1,17 @@
 #!/home/mc/python3.5.1/bin/python3.5
+'''
+This is a short test script which is build to check whether the bot can
+receive and send messages in a given server, note that a token file must
+be present for this script to work
+'''
 
-import discord, asyncio
+import discord, asyncio, sys, os
 client = discord.Client()
 
 @client.event
 async def on_message(message):
     print('received message')
-    print(type(message.server))
-    if message.server.name == 'Test' and message.author.name != 'Tournament-Bot':
+    if message.server.name == SERVER and message.author.name != 'Tournament-Bot':
         print('sending message')
         await client.send_message(message.channel, 'I live!')
 
@@ -18,5 +22,17 @@ async def on_ready():
     print(client.user.id)
     print('-----')
 
-client.run('MTg5NDgzNjU5NzkzOTI0MDk2.CmFpPA.wAM7k97fqlEI5kb0OPDwj4j8pOE')
-client.connect()
+if __name__ == '__main__':
+    INPUT = sys.argv[1:]
+    if INPUT:
+        SERVER = ' '.join(INPUT)
+        if os.path.isfile('token.txt'):
+            token = open('token.txt').readlines()[0].strip()
+            client.run(token)
+            client.connect()
+        else:
+            print('There is no token file present, make sure your file is named "token.txt"')
+            sys.exit()
+    else:
+        print('You must input a server to be used')
+        sys.exit()

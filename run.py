8ce4bc984631a10dbcb7e2f6770bbd2766683765
MarkTheF4rth <mnel.jnr@gmail.com>
2016-08-10 02:20:26 +0100

    Removed Main class from run, added functionality to let main loop print out messages

--- run.py
+++ run.py
@@ -1,29 +1,18 @@
-#!/home/mc/python3.5.1/bin/python3.5
-
 import discord, asyncio, sys, os, shelve, time
+from commands import Main
 client = discord.Client()
 
 LOOP = asyncio.get_event_loop()
 
-class Main:
-    def __init__(self):
-        self.config = shelve.open('Configs/config-MASTER')
-        self.in_messages = []
-        self.out_messages = []
-   
-    def message_handler(self):
-        for message in self.in_messages:
-            print('Processed message: ', message.content, message.channel)
-
 @client.event
 async def on_message(message):
     if message.content.lstrip().startswith(MAIN.config['command_char']): #removes leading spaces and checks that the message begins with the command character
-        MAIN.in_messages.append(message)
+        MAIN.message_parser(message)
     
 @client.event
 async def on_message_edit(old, message):
     if message.content.lstrip().startswith(MAIN.config['command_char']):
-        MAIN.in_messages.append(message)
+        MAIN.message_parser(message, True)
 
 @client.event
 async def on_ready():
@@ -37,9 +26,12 @@ async def main_loop(main):
     await client.send_message(main.config['message'].channel, 'I have restarted')
     while not client.is_closed:
         main.message_handler()
+        for message in main.out_messages:
+            await client.send_message(message[0], message[1])
+            main.out_messages.pop(0)
         await asyncio.sleep(1)
 
-MAIN = Main()
+MAIN = Main(shelve)
 if __name__ == '__main__':
     if not os.path.isfile('token.txt'):
         print('There is no token file present, make sure your file is named "token.txt"')
